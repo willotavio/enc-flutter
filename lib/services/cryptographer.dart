358f9594e6e61a2c9467dab8280c1ba3ede1e9d5
willotavio <willaotavio@gmail.com>
2024-01-31 14:01:13 -0300

    Fix encryption and decryption padding scheme

--- lib/services/cryptographer.dart
+++ lib/services/cryptographer.dart
@@ -4,19 +4,18 @@ import "package:pointycastle/pointycastle.dart";
 
 class Cryptographer{
   
-  static String encrypt(String textToEncrypt, String encryptionKey, String iv){
-    if(textToEncrypt.isNotEmpty && encryptionKey.length == 16){
-
+  static String encrypt(String textToEncrypt, String encryptionKey, String encryptionIv){
+    if(textToEncrypt.isNotEmpty && encryptionKey.length == 16 && encryptionIv.length == 16){
       final Uint8List keyBytes = Uint8List.fromList(utf8.encode(encryptionKey));
-      final Uint8List ivBytes = Uint8List.fromList(utf8.encode(iv));
-      final params = ParametersWithIV(KeyParameter(keyBytes), ivBytes);
+      final Uint8List ivBytes = Uint8List.fromList(utf8.encode(encryptionIv));
+      final params = PaddedBlockCipherParameters(ParametersWithIV(KeyParameter(keyBytes), ivBytes), null);
 
-      final BlockCipher cipher = BlockCipher("AES/CBC")..init(true, params);
+      final BlockCipher cipher = PaddedBlockCipher("AES/CBC/PKCS7");
+      cipher.init(true, params);
       
       final textBytes = utf8.encode(textToEncrypt);
-      final paddedTextBytes = _padText(textBytes);
 
-      final encryptedBytes = Uint8List.fromList(cipher.process(paddedTextBytes));
+      final encryptedBytes = Uint8List.fromList(cipher.process(textBytes));
       final encryptedText = base64.encode(encryptedBytes);
       return encryptedText;
     }
@@ -25,18 +24,17 @@ class Cryptographer{
       if(textToEncrypt.isEmpty){
         message = "Provide something to be encrypted (`>-л-)>";
       }
-      else if(encryptionKey.length < 16 || encryptionKey.length > 16){
+      else if(encryptionKey.length != 16){
         message = "The key must have 16 characters (`>-л-)>";
+      }
+      else if(encryptionIv.length != 16){
+        message = "The IV must have 16 characters (`>-л-)>";
+      }
+      else if(encryptionKey == encryptionIv){
+        message = "The IV and the key must not be equal \\(-д-\\*)";
       }
       return message;
     }
-  }
-
-  static Uint8List _padText(Uint8List text){
-    final blockSize = 16;
-    final padLength = blockSize - (text.length % blockSize);
-    final paddedText = Uint8List(text.length + padLength)..setAll(0, text);
-    return paddedText;
   }
 
   static String decrypt(String encryptedText, String decryptionKey, String decryptionIv){
@@ -45,15 +43,13 @@ class Cryptographer{
       final ivBytes = Uint8List.fromList(utf8.encode(decryptionIv));
       final encryptedTextBytes = base64.decode(encryptedText);
 
-      final params = ParametersWithIV(KeyParameter(keyBytes), ivBytes);
+      final params = PaddedBlockCipherParameters(ParametersWithIV(KeyParameter(keyBytes), ivBytes), null);
 
-      final cipher = BlockCipher("AES/CBC")..init(false, params);
+      final cipher = PaddedBlockCipher("AES/CBC/PKCS7")..init(false, params);
 
       final decryptedBytes = Uint8List.fromList(cipher.process(encryptedTextBytes));
-      final unpaddedTextBytes = _unpadText(decryptedBytes);
 
-      return utf8.decode(unpaddedTextBytes);
-
+      return utf8.decode(decryptedBytes);
     }
     else{
       String message = "";
@@ -71,10 +67,5 @@ class Cryptographer{
       }
       return message;
     }
-  }
-
-  static Uint8List _unpadText(Uint8List text){
-    final padLength = text.last;
-    return Uint8List.sublistView(text, 0, text.length - padLength);
   }
 }
