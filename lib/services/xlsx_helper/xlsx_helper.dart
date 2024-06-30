0b9b450b1bd66ba4e1d75d7a434cd3fb3d321f8f
willotavio <willaotavio@gmail.com>
2024-06-29 23:46:52 -0300

    Add XlsxHelper

---
+++ lib/services/xlsx_helper/xlsx_helper.dart
@@ -0,0 +1,31 @@
+import 'package:enc_flutter/services/encrypted_text/encrypted_text.dart';
+import 'package:excel/excel.dart';
+
+class XlsxHelper {
+
+  static Excel getEncryptedTextsXLSX(List<EncryptedText> encryptedTextsList) {
+    Excel xlsx = Excel.createExcel();
+    var encryptedTextsSheet = xlsx["Encrypted Texts"];
+    var rows = [
+      TextCellValue("ID"),
+      TextCellValue("Title"),
+      TextCellValue("Encrypted Text"),
+      TextCellValue("Description")
+    ];
+    encryptedTextsSheet.appendRow(rows);
+    
+    for(var encryptedText in encryptedTextsList) {
+      encryptedTextsSheet.appendRow(
+        [
+          TextCellValue(encryptedText.id),
+          TextCellValue(encryptedText.title),
+          TextCellValue(encryptedText.description ?? "No description"),
+          TextCellValue(encryptedText.encryptedText),
+        ],
+      );
+    }
+
+    return xlsx;
+  }
+
+}
