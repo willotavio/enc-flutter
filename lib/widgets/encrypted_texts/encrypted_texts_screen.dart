1faf7df65f2733e10579e985583031f934516223
willotavio <willaotavio@gmail.com>
2024-07-08 23:03:51 -0300

    Add folder selection with FilePicker when saving XLSX in EncryptedTextsScreen

--- lib/widgets/encrypted_texts/encrypted_texts_screen.dart
+++ lib/widgets/encrypted_texts/encrypted_texts_screen.dart
@@ -4,6 +4,7 @@ import 'package:enc_flutter/services/permission_helper/permission_helper.dart';
 import 'package:enc_flutter/services/xlsx_helper/xlsx_helper.dart';
 import 'package:enc_flutter/widgets/encrypted_texts/encrypted_texts_list.dart';
 import 'package:excel/excel.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 class EncryptedTextsScreen extends StatefulWidget {
@@ -22,21 +23,33 @@ class _EncryptedTextsListState extends State<EncryptedTextsScreen> {
           onPressed: () async {
             bool permissionGranted = await PermissionHelper.requestStoragePermission();
             if(permissionGranted) {
-              Excel xlsx = XlsxHelper.getEncryptedTextsXLSX(await EncryptedTextService.getEncryptedTexts());
-              String pathToSave = "storage/emulated/0/Download";
-              var now = DateTime.now();
-              File file = File("$pathToSave/encrypted-texts-${now.hour}${now.minute}${now.second}.xlsx");
-              var saved = xlsx.save();
-              if(saved != null) {
-                file.writeAsBytes(saved);
+              String? pathToSave = await FilePicker.platform.getDirectoryPath();
+              if(pathToSave != null) {
+                Excel xlsx = XlsxHelper.getEncryptedTextsXLSX(await EncryptedTextService.getEncryptedTexts());
+                var now = DateTime.now();
+                File file = File("$pathToSave/encrypted-texts-${now.year}-${now.month}-${now.day}-${now.hour}-${now.minute}-${now.second}.xlsx");
+                var saved = xlsx.save();
+                if(saved != null) {
+                  file.writeAsBytes(saved);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text("XLSX saved in the chosen folder (/>v<)/!"),
+                      duration: Duration(seconds: 1),
+                      dismissDirection: DismissDirection.horizontal,
+                      showCloseIcon: true,
+                    ),
+                  );
+                }
+              }
+              else {
                 ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text("XLSX saved in downloads folder (/>v<)/!"),
-                    duration: Duration(seconds: 1),
-                    dismissDirection: DismissDirection.horizontal,
-                    showCloseIcon: true,
-                  ),
-                );
+                    SnackBar(
+                      content: Text("XLSX not saved (/-д-)_!"),
+                      duration: Duration(seconds: 1),
+                      dismissDirection: DismissDirection.horizontal,
+                      showCloseIcon: true,
+                    ),
+                  );
               }
             }
             else {
